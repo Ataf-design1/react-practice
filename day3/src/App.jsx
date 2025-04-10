bd7d151b8049e55c7a26f739510682baaa0638ce
Ataf_Khan <atafkhan980@gmail.com>
2025-04-10 16:10:23 +0530

    contextapi

--- day3/src/App.jsx
+++ day3/src/App.jsx
@@ -1,12 +1,24 @@
-import React from 'react'
-import Todo from './component/Todo'
+import React from "react";
+import Todo from "./component/Todo";
+import { Routes, Route } from "react-router-dom";
+import Page1 from "./pages/Page1";
+import Page2 from "./pages/Page2";
+import Navbar from "./component/Navbar";
+import Page3 from "./pages/Page3";
 
 const App = () => {
   return (
     <div>
-      <Todo/>
+      <Navbar/>
+      <Routes>
+        <Route path="/" element={<Todo />} />
+        <Route path="/p1" element={<Page1 />} />
+        <Route path="/p2" element={<Page2 />} />
+        <Route path="/p3" element={<Page3/>} />
+      </Routes>
+      
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
